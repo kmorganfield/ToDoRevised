922a575dc8c9da1f88c4992ffd6079c47c4d7b68
kmorganfield <kennedymorganfield@gmail.com>
2019-07-15 13:20:45 -0500

    Initial Commit

---
+++ ToDoProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoProject
+//
+//  Created by Kennedy Morganfield on 7/15/19.
+//  Copyright © 2019 Kennedy Morganfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
